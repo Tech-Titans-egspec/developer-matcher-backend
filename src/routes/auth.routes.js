d99395254a4cf30fb5b4f1bae483d08ffb5940e9
ARISHKANNAN A <161874829+Arish5Kannan@users.noreply.github.com>
2025-08-16 18:34:53 +0530

    Delete src directory
    hgfhhgf

--- src/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { registerUser,login } from "../controllers/auth.controller.js";
-
-const router = express.Router();
-
-router.post("/register", registerUser);
-router.post('/login', login);
-
-export default router;
